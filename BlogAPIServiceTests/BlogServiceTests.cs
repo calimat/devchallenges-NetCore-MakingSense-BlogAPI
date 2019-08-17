01514022f8de517646caf539c64472f9bb89bfad
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-08-17 15:19:58 -0500

    Starting with the get feature of lists of posts for the API Service

---
+++ BlogAPIServiceTests/BlogServiceTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class BlogService
+    {
+        public List<Post> Get()
+        {
+            return new List<Post>();
+        }
+    }
+
+    public class Post
+    {
+
+    }
+
+    public class BlogServiceTests
+    {
+        [Test]
+        public void BlogService_ShouldNotBeNull()
+        {
+            var sut =  new BlogService();
+            Assert.IsNotNull(sut);
+        }
+
+       
+        [Test]
+        public void BlogService_ShouldReturnAListOfPosts()
+        {
+            var sut = new BlogService();
+            var result = sut.Get();
+            Assert.IsInstanceOf<List<Post>>(result);
+        }
+    }
+}
